d0dd06bf88381e2cb0ee5ec37c381e78e83e38d2
Antonio Claudio <claudioalvesdev@gmail.com>
2026-06-05 08:29:34 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Paycheck",
+  description: "Paycheck",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" className={`${poppins.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-background">
+      <h1>Home</h1>
+    </main>
+  );
+}
